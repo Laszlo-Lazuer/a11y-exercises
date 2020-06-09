cea51f532596dc067752956025b4c389e9dc047e
Laszlo Lazuer <visualjazz@gmail.com>
2020-06-09 18:31:23 -0500

    Updates

---
+++ precache-manifest.bfadf9e2e49f7df4f7671a1acdf40f87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cbef495a1a9f07ecc96b19dc2c7b27d",
+    "url": "/a11y-exercises/index.html"
+  },
+  {
+    "revision": "d9f7162d2e6a260349ce",
+    "url": "/a11y-exercises/static/css/main.b7b6fbb9.chunk.css"
+  },
+  {
+    "revision": "3ded022c4423efc51de8",
+    "url": "/a11y-exercises/static/js/2.d39b828b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/a11y-exercises/static/js/2.d39b828b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f7162d2e6a260349ce",
+    "url": "/a11y-exercises/static/js/main.d051d21e.chunk.js"
+  },
+  {
+    "revision": "7d5e7dd415dfc6a2b6c1",
+    "url": "/a11y-exercises/static/js/runtime-main.7666b2da.js"
+  }
+]);
