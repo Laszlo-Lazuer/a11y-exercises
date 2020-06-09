deb0e28c1b2a27d456620886b674b842dc5da679
Laszlo Lazuer <visualjazz@gmail.com>
2020-06-09 18:30:17 -0500

    feature: adds nav and example pages.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.css';
+
+import Home from './Pages/Home/index';
+import Exercise1 from './Pages/Exercise1/index';
+import Exercise2 from './Pages/Exercise2/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <main>
+          <Switch>
+            <Route path="/ex1">
+              <Exercise1 />
+            </Route>
+            <Route path="/ex2">
+              <Exercise2 />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
   );
 }
 
---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import Nav from "../../components/Nav";
+
+const Home = () => {
+    return <Fragment>
+        <h1>A11y Exercise Portal</h1>
+        <Nav />
+    </Fragment>;
+};
+
+export default Home;
